df3fbf3ae961a5aae700955ff9d99bd02e450019
jofoks <joormillenaar@live.nl>
2024-08-31 16:04:50 -0700

    Added missing media components

---
+++ src/components/media/SimpleMacMockup.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="device-mockup">
+    <img
+      src="@/assets/images/simple-mac.webp"
+      alt="MacBook Mockup"
+      class="device-image"
+      loading="lazy"
+    />
+    <object class="screen-content">
+      <video-content :video="video"></video-content>
+    </object>
+  </div>
+</template>
+
+<script>
+import VideoContent from './VideoContent.vue';
+export default {
+  components: {
+    VideoContent,
+  },
+  props: ['video'],
+};
+</script>
+
+<style scoped>
+.device-mockup {
+  position: relative;
+  width: 100%;
+  height: auto;
+}
+
+.device-image {
+  width: 100%;
+  display: block;
+}
+
+.screen-content {
+  position: absolute;
+  top: 4.4%;
+  left: 11.5%;
+  width: 77%;
+  height: 85.5%;
+}
+
+/* .screen-content img, */
+/* .screen-content video { */
+/* object-fit: contain; */
+/* } */
+</style>
---
+++ src/components/media/VideoContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <video class="box" muted loop playsinline autoplay loading="lazy">
+    <source :src="video" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+</template>
+
+<script>
+export default {
+  props: {
+    video: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  box-shadow: var(--shadow);
+}
+</style>
