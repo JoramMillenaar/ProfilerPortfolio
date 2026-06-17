9609345feae9e8f0603b464b542bc47f0c19db22
JoramMillenaar <jlmillenaar@gmail.com>
2026-06-17 11:37:05 -0700

    Replaced custom content format with MD

--- src/components/common/CustomText.vue
+++ src/components/common/CustomText.vue
@@ -1,62 +1,50 @@
 <template>
-  <div>
-    <div v-for="(section, index) in content" :key="index" class="text-box">
-      <h3 v-if="section.type === 'header'" class="pt-gutter-medium pb-gutter-nano text-xl font-semibold">
-        {{ section.content }}
-      </h3>
-
-      <p v-else-if="section.type === 'paragraph'">
-        {{ section.content }}
-      </p>
-
-      <ul v-else-if="section.type === 'list'">
-        <li v-for="(item, i) in section.items" :key="i">
-          {{ item }}
-        </li>
-      </ul>
-
-      <figure v-else-if="section.type === 'image'" class="image-block shadow">
-        <img :src="section.src" :alt="section.alt || ''"/>
-        <figcaption v-if="section.caption">{{ section.caption }}</figcaption>
-      </figure>
-
-      <p v-else class="unknown">
-        Unknown section type: {{ section.type }}
-      </p>
-    </div>
-  </div>
+  <div class="markdown-body" v-html="renderedContent"></div>
 </template>
 
 <script>
+import { marked } from 'marked';
+
 export default {
   name: 'CustomText',
   props: {
     content: {
-      type: Array,
+      type: String,
       required: true,
-      default: () => [],
+      default: '',
+    },
+  },
+  computed: {
+    renderedContent() {
+      return marked.parse(this.content || '');
     },
   },
 };
 </script>
 
 <style scoped>
-.text-box {
+.markdown-body :deep(h1),
+.markdown-body :deep(h2),
+.markdown-body :deep(h3),
+.markdown-body :deep(h4) {
+  padding-top: var(--gutter-medium);
+  padding-bottom: var(--gutter-nano);
+  text-align: left;
+  font-size: 1.25rem;
+  font-weight: 600;
+}
+
+.markdown-body :deep(p) {
   padding-top: var(--gutter-micro);
 }
 
-.subheader {
-  padding-top: var(--gutter-medium);
-  padding-bottom: var(--gutter-nano);
-}
-
-ul {
+.markdown-body :deep(ul) {
   list-style: none;
   margin-left: 0;
   padding-left: 1em;
 }
 
-ul > li:before {
+.markdown-body :deep(ul > li):before {
   display: inline-block;
   padding-top: var(--gutter-nano);
   content: '-';
@@ -64,31 +52,24 @@ ul > li:before {
   margin-left: -1em;
 }
 
-.image-block {
-  margin: 0;
-  padding-bottom: var(--gutter-medium);
-}
-
-.image-block img {
+.markdown-body :deep(img) {
   display: block;
-  max-width: 100%;
+  max-width: 90%;
+  padding: var(--gutter-medium);
   margin: auto;
   height: auto;
-}
-
-.image-block figcaption {
-  padding-top: var(--gutter-nano);
-  font-size: 0.875em;
-  opacity: 0.7;
-  font-style: italic;
-}
-
-.shadow {
   filter: drop-shadow(0px 0px 172px rgba(255, 255, 255, 0.1));
 }
 
-.unknown {
-  opacity: 0.7;
+.markdown-body :deep(blockquote) {
+  border-left: 3px solid var(--color-border, rgba(255, 255, 255, 0.2));
+  margin: var(--gutter-micro) 0;
+  padding: var(--gutter-nano) var(--gutter-small, 1rem);
+  opacity: 0.75;
+}
+
+.markdown-body :deep(blockquote p) {
+  padding-top: 0;
   font-style: italic;
 }
 </style>
