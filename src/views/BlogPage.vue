f242090b2a92bfa857fda2fa7b3b8f98665c9117
JoramMillenaar <jlmillenaar@gmail.com>
2026-06-19 10:42:00 -0700

    Added new blog post

--- src/views/BlogPage.vue
+++ src/views/BlogPage.vue
@@ -107,7 +107,7 @@ export default {
 
 .posts-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
   gap: var(--gutter-medium);
 }
 
